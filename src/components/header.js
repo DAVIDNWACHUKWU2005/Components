cced7919a2b83d95b0b8f66f84ded74f70d5608d
David Nwachukwu <davidike444@gmail.com>
2026-06-03 22:25:30 -0500

    Added header component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Gallery</h1>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+      </nav>
+    </header>
+  );
+}
+ 
+export default Header;
